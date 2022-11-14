f6e26c08183d830fb82d999a1a9d944fcc1aa256
Chahid Adnane <adnanechahid@gmail.com>
2022-11-14 02:54:18 +0100

    feature/create articles modules and add routing using lazy-loading

--- my-blog/src/app/app-routing.module.ts
+++ my-blog/src/app/app-routing.module.ts
@@ -3,7 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: 'contact', component: ContactComponent }
+  // first match wins strategy to select one
+  { path: 'contact', component: ContactComponent },
+  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
+  // default route
+  { path: '', pathMatch: 'full', redirectTo: 'articles' },
+  // wildcard route
+  { path: '**', redirectTo: 'articles' }
 ];
 
 @NgModule({
