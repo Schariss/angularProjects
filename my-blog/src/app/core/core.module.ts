8010dc8009690e87ba6778de7e071fa39351c789
Chahid Adnane <adnanechahid@gmail.com>
2022-11-14 02:12:21 +0100

    feature/add routing the the core module

--- my-blog/src/app/core/core.module.ts
+++ my-blog/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,10 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    // The routerLink and routerLinkActive directives are part of the Angular router package. 
+    // We need to import RouterModule in the core module to use them
+    RouterModule
   ],
   exports: [
     // So that other modules can use it
