32c9ebd9f48d16b3972cb0d50957b2a6d8ad0ac8
Chahid Adnane <adnanechahid@gmail.com>
2022-11-15 23:36:27 +0100

    feature/display posts on the home page

--- my-blog/src/app/articles/articles.component.ts
+++ my-blog/src/app/articles/articles.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-articles',
   templateUrl: './articles.component.html',
   styleUrls: ['./articles.component.scss']
 })
-export class ArticlesComponent implements OnInit {
+export class ArticlesComponent implements OnInit, OnDestroy {
 
   posts: ScullyRoute[] = [];
+  private routeSub: Subscription | undefined;
 
   constructor(private scullyService: ScullyRoutesService) { }
 
+  // Angular services that provide initialization logic to a component should be
+  // called inside the ngOnInit method and not in the constructor because it is
+  // easier to provide mocks about those services when unit testing the component
   ngOnInit(): void {
-    this.scullyService.available$.subscribe(posts => {
-      this.posts = posts.filter(post => post.title);
+    // this.routeSub = 
+      this.scullyService.available$.subscribe(posts => {
+        this.posts = posts.filter(post => post.title);
       });
   }
+
+  ngOnDestroy(): void {
+    this.routeSub?.unsubscribe();
+    }
 
 }
