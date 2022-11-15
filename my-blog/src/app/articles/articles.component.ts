aaeec1e2c8b507971de415e765464f8373b97e5a
Chahid Adnane <adnanechahid@gmail.com>
2022-11-16 00:44:32 +0100

    feature/add scully functionality to start creating posts

--- my-blog/src/app/articles/articles.component.ts
+++ my-blog/src/app/articles/articles.component.ts
@@ -12,14 +12,21 @@ export class ArticlesComponent implements OnInit, OnDestroy {
   posts: ScullyRoute[] = [];
   private routeSub: Subscription | undefined;
 
+  // Scully provides 'ScullyRoutesService', an Angular service that we can use in our components to get
+  // information about the routes that it will create according to the blog posts
   constructor(private scullyService: ScullyRoutesService) { }
 
   // Angular services that provide initialization logic to a component should be
   // called inside the ngOnInit method and not in the constructor because it is
   // easier to provide mocks about those services when unit testing the component
   ngOnInit(): void {
-    // this.routeSub = 
+    // The returned posts variable will contain all the available routes that were generated from Scully
+    // Scully is run against all routes of our Angular application
+
+    //this.routeSub = 
       this.scullyService.available$.subscribe(posts => {
+        // To avoid displaying routes other than those related to blog posts, 
+        // such as the contact route, we filter out the results from the available$ property
         this.posts = posts.filter(post => post.title);
       });
   }
