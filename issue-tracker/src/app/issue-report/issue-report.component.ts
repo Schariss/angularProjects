e2f824e52aef9637f192038eb8b2ab9c560b35a2
Chahid Adnane <adnanechahid@gmail.com>
2022-11-16 03:06:04 +0100

    feature/report a new issue

--- issue-tracker/src/app/issue-report/issue-report.component.ts
+++ issue-tracker/src/app/issue-report/issue-report.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { IssuesService } from '../issues.service';
 
 @Component({
   selector: 'app-issue-report',
@@ -8,10 +9,25 @@ import { FormBuilder } from '@angular/forms';
 })
 export class IssueReportComponent implements OnInit {
 
+  issueForm: FormGroup | undefined;
+
   // FormBuilder is an Angular service that we use to build a reactive form in an easy and convenient way
-  constructor(private builder: FormBuilder) { }
+  constructor(private builder: FormBuilder, private issueService: IssuesService) { }
 
   ngOnInit(): void {
+    // FormGroup is used to group individual controls into a logical representation of a form
+    this.issueForm = this.builder.group({
+      title: [''],
+      description: [''],
+      priority: [''],
+      type: [''],
+    });
+  }
+
+  addIssue() {
+    // The value property of a FormGroup object contains the model of the form
+    // The keys of the model match the property names of the Issue interface
+    this.issueService.createIssue(this.issueForm?.value);
   }
 
 }
